7194691559b7e44ce1d9ff3a8c0fb739e1337ce7
MeronDT <merondebt@gmail.com>
2024-03-05 17:31:11 -0800

    mt - fix typo in import

--- frontend/src/main/pages/CourseDetails/CourseDetailsIndexPage.js
+++ frontend/src/main/pages/CourseDetails/CourseDetailsIndexPage.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { useBackend, _useBackendMutation } from "main/utils/useBackend";
 import CourseDetailsTable from "main/components/CourseDetails/CourseDetailsTable";
 import { yyyyqToQyy } from "main/utils/quarterUtilities";
-import CourseDescriptionTable from "main/components/Coursedeatils/CourseDescriptionTable";
+import CourseDescriptionTable from "main/components/Courses/CourseDescriptionTable";
 
 export default function CourseDetailsIndexPage() {
   // Stryker disable next-line all : Can't test state because hook is internal
