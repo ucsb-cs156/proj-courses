75f1467922c73627aa4f15a9d9e6a0c8f85ea115
jiaqiguan2003 <jiaqiguan@ucsb.edu>
2024-03-05 20:31:35 -0800

    fix not working link

--- frontend/src/main/pages/Courses/PSCourseCreatePage.js
+++ frontend/src/main/pages/Courses/PSCourseCreatePage.js
@@ -41,7 +41,7 @@ export default function CoursesCreatePage() {
     return <Navigate to="/courses/list" />;
   }
   if (mutation.isError) {
-    if (mutation.error.response.data?.message.includes("psId")){
+    if (mutation.error.response.data?.message.includes("psId")) {
       return (
         <BasicLayout>
           <div className="pt-2">
@@ -49,28 +49,30 @@ export default function CoursesCreatePage() {
   
             <CourseForm submitAction={onSubmit} />
             <p data-testid="PSCourseCreate-Error">
-                Error: No personal schedules found. Please{" "}
-                first.
-            </p>
-          </div>
-        </BasicLayout>
-      );
-    } else {
-      return (
-        <BasicLayout>
-          <div className="pt-2">
-            <h1>Create New Course</h1>
-
-            <CourseForm submitAction={onSubmit} />
-            <p data-testid="PSCourseCreate-Error">
-              Error: {mutation.error.response.data?.message}
+              Error: No personal schedules found. Please{" "}
+              <span style={{ color: "blue", cursor: "pointer" }} onClick={() => window.location.href = "/personalschedules/create"}>
+                create a new personal schedule
+              </span>{" "}
+              first.
             </p>
           </div>
         </BasicLayout>
       );
     }
-    
+    return (
+      <BasicLayout>
+        <div className="pt-2">
+          <h1>Create New Course</h1>
+
+          <CourseForm submitAction={onSubmit} />
+          <p data-testid="PSCourseCreate-Error">
+            Error: {mutation.error.response.data?.message}
+          </p>
+        </div>
+      </BasicLayout>
+    );
   }
+
   return (
     <BasicLayout>
       <div className="pt-2">
