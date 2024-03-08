5925f419abe97c6e7e87b856e9c001420dde59bb
jiaqiguan2003 <jiaqiguan@ucsb.edu>
2024-03-07 17:55:25 -0800

    added back button

--- frontend/src/main/pages/PersonalSchedules/PersonalSchedulesEditPage.js
+++ frontend/src/main/pages/PersonalSchedules/PersonalSchedulesEditPage.js
@@ -1,11 +1,20 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { Button } from "react-bootstrap";
 
 export default function PersonalSchedulesEditPage() {
+  const createButton = () => {
+    return (
+      <Button variant="primary" href="/personalschedules/list" style={{}}>
+        Back
+      </Button>
+    );
+  };
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Personal Schedules</h1>
         <p>This is where the edit page will go</p>
+        {createButton()}
       </div>
     </BasicLayout>
   );
