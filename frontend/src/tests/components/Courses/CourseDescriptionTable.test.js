1c15a950711a1d48117969ef65d2166f56eb2ecc
jiaqiguan2003 <124765197+jiaqiguan2003@users.noreply.github.com>
2024-03-08 22:16:42 -0800

    Update CourseDescriptionTable.test.js

--- frontend/src/tests/components/Courses/CourseDescriptionTable.test.js
+++ frontend/src/tests/components/Courses/CourseDescriptionTable.test.js
@@ -1,61 +1,18 @@
 import { render, screen } from "@testing-library/react";
-import { coursesFixtures } from "fixtures/courseFixtures";
-import BasicDescriptionTable from "main/components/Courses/BasicDescriptionTable";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { MemoryRouter } from "react-router-dom";
+import BasicDescriptionTable from "main/components/Courses/CourseDescriptionTable";
 
-const mockedNavigate = jest.fn();
+const mockCourse = {
+  description: "Test course description",
+};
 
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useNavigate: () => mockedNavigate,
-}));
-
-describe("DescriptionTable tests", () => {
-  const queryClient = new QueryClient();
-
-  test("renders without crashing for empty table", () => {
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <BasicDescriptionTable courses={[]} />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
+describe("CourseDescriptionTable tests", () => {
+  test("renders without crashing", () => {
+    render(<BasicDescriptionTable course={mockCourse} />);
   });
 
-  test("Has the expected column headers and content", () => {
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <BasicDescriptionTable courses={coursesFixtures.twoCourses} />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
-
-    const expectedHeaders = [
-      "Description",
-    ];
-    const expectedFields = [
-      "description",
-    ];
-    const testId = "BasicDescriptionTable";
-
-    expectedHeaders.forEach((headerText) => {
-      const header = screen.getByText(headerText);
-      expect(header).toBeInTheDocument();
-    });
-
-    expectedFields.forEach((field) => {
-      const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
-      expect(header).toBeInTheDocument();
-    });
-
-    expect(
-      screen.getByTestId(`${testId}-cell-row-0-col-quarter`),
-    ).toHaveTextContent("W21");
-    expect(
-      screen.getByTestId(`${testId}-cell-row-1-col-courseId`),
-    ).toHaveTextContent("CMPSC 16");
+  test("displays the course description", () => {
+    render(<BasicDescriptionTable course={mockCourse} />);
+    expect(screen.getByText("Course Description")).toBeInTheDocument();
+    expect(screen.getByText("Test course description")).toBeInTheDocument();
   });
 });
