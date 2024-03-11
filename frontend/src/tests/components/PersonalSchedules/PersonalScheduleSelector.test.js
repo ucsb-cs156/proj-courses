52a4f5972a873461fbb30aee4a1daf5ded0db5f4
Jonathan Herring <jonathan_herring@ucsb.edu>
2024-03-10 19:07:52 -0700

    Fix ESLine Errors

--- frontend/src/tests/components/PersonalSchedules/PersonalScheduleSelector.test.js
+++ frontend/src/tests/components/PersonalSchedules/PersonalScheduleSelector.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import PersonalScheduleSelector from 'main/components/PersonalSchedules/PersonalScheduleSelector';
 import { useBackend } from 'main/utils/useBackend';
 
@@ -42,18 +41,16 @@ describe('PersonalScheduleSelector', () => {
   });
 
   it('updates schedule state and localStorage on select change', async () => {
-    const { getByLabelText } = render(<PersonalScheduleSelector setSchedule={setSchedule} setHasSchedules={setHasSchedules} controlId="test" />);
-    const select = getByLabelText('Schedule');
-    act(() => {
-      fireEvent.change(select, { target: { value: '1' } });
-    });
+    render(<PersonalScheduleSelector setSchedule={setSchedule} setHasSchedules={setHasSchedules} controlId="test" />);
+    const select = screen.getByLabelText('Schedule');
+    fireEvent.change(select, { target: { value: '1' } });
     expect(localStorage.getItem('test')).toBe('1');
     expect(setSchedule).toHaveBeenCalledWith('1');
   });
-  
+
   it('calls the provided onChange function when the select changes', () => {
     const onChange = jest.fn();
-    const { getByLabelText } = render(
+    render(
       <PersonalScheduleSelector
         setSchedule={setSchedule}
         setHasSchedules={setHasSchedules}
@@ -61,20 +58,14 @@ describe('PersonalScheduleSelector', () => {
         onChange={onChange}
       />
     );
-    const select = getByLabelText('Schedule');
+    const select = screen.getByLabelText('Schedule');
     fireEvent.change(select, { target: { value: '1' } });
     expect(onChange).toHaveBeenCalled();
   });
-  
+
   it('does not call onChange if it is null', () => {
-    const { getByLabelText } = render(
-      <PersonalScheduleSelector
-        setSchedule={setSchedule}
-        setHasSchedules={setHasSchedules}
-        controlId="test"
-      />
-    );
-    const select = getByLabelText('Schedule');
+    render(<PersonalScheduleSelector setSchedule={setSchedule} setHasSchedules={setHasSchedules} controlId="test" />);
+    const select = screen.getByLabelText('Schedule');
     fireEvent.change(select, { target: { value: '1' } });
     // No assertion necessary as we're just verifying that no error is thrown
   });
