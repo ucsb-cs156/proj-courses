ada36bdbff4470e637f4de1e121dff7a85e99dac
Andrew <andrew344c@gmail.com>
2024-05-21 12:38:55 -0700

    Fix fixtures file formatting

--- frontend/src/fixtures/courseGradeDistFixtures.js
+++ frontend/src/fixtures/courseGradeDistFixtures.js
@@ -1,48 +1,48 @@
 const gradeDistFixtures = {
-    oneGradeDist: [
-        {
-            id: 1,
-            yyyyq: "20234",
-            course: "CMPSC 130A",
-            instructor: "Joe Test1",
-            grade: "A-",
-            count: 1,
-            subjectArea: "CMPSC",
-            courseNum: "130A"
-        }
-    ],
-    threeGradeDist: [
-        {
-            id: 1,
-            yyyyq: "20231",
-            course: "CMPSC 130B",
-            instructor: "Bob Test1",
-            grade: "B+",
-            count: 3,
-            subjectArea: "CMPSC",
-            courseNum: "130B"
-        },
-        {
-            id: 2,
-            yyyyq: "20232",
-            course: "PSTAT 120B",
-            instructor: "Jane Test2",
-            grade: "A+",
-            count: 2,
-            subjectArea: "PSTAT",
-            courseNum: "120B"
-        },
-        {
-            id: 3,
-            yyyyq: "20233",
-            course: "CMPSC 156",
-            instructor: "Jack Test3",
-            grade: "A-",
-            count: 1,
-            subjectArea: "CMPSC",
-            courseNum: "156"
-        }
-    ]
-}
+  oneGradeDist: [
+    {
+      id: 1,
+      yyyyq: "20234",
+      course: "CMPSC 130A",
+      instructor: "Joe Test1",
+      grade: "A-",
+      count: 1,
+      subjectArea: "CMPSC",
+      courseNum: "130A",
+    },
+  ],
+  threeGradeDist: [
+    {
+      id: 1,
+      yyyyq: "20231",
+      course: "CMPSC 130B",
+      instructor: "Bob Test1",
+      grade: "B+",
+      count: 3,
+      subjectArea: "CMPSC",
+      courseNum: "130B",
+    },
+    {
+      id: 2,
+      yyyyq: "20232",
+      course: "PSTAT 120B",
+      instructor: "Jane Test2",
+      grade: "A+",
+      count: 2,
+      subjectArea: "PSTAT",
+      courseNum: "120B",
+    },
+    {
+      id: 3,
+      yyyyq: "20233",
+      course: "CMPSC 156",
+      instructor: "Jack Test3",
+      grade: "A-",
+      count: 1,
+      subjectArea: "CMPSC",
+      courseNum: "156",
+    },
+  ],
+};
 
 export { gradeDistFixtures };
