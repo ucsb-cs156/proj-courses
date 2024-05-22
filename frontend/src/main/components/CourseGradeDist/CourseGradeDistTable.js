9fc081500fb6052fd0788c47e43b5bbf24d3d4d2
Andrew <andrew344c@gmail.com>
2024-05-21 20:52:46 -0700

    Revert "Added very basic course grade dist table and tests"
    This reverts commit f5c124c2cdd1cbf345bc1c500b75390df47615f3.
    
    I accidently commited this here. It should have been in another branch.

--- frontend/src/main/components/CourseGradeDist/CourseGradeDistTable.js
+++ frontend/src/main/components/CourseGradeDist/CourseGradeDistTable.js
@@ -1,60 +1,11 @@
-import OurTable from "../OurTable";
-
 // eslint-disable-next-line no-unused-vars
 export default function CourseGradeDistTable({ gradeData }) {
-  const columns = [
-    {
-      Header: "id",
-      accessor: "id",
-    },
-    {
-      Header: "Session",
-      accessor: "session",
-    },
-    {
-      Header: "Course",
-      accessor: "course",
-    },
-    {
-      Header: "Instructor",
-      accessor: "instructor",
-    },
-    {
-      Header: "Grade",
-      accessor: "grade",
-    },
-    {
-      Header: "Count",
-      accessor: "count",
-    },
-  ];
-
-  for (const gradeDist of gradeData) {
-    const year = gradeDist.yyyyq.slice(0, 4);
-    let quarter;
-    switch (gradeDist.yyyyq[4]) {
-      case "1":
-        quarter = "Winter";
-        break;
-      case "2":
-        quarter = "Spring";
-        break;
-      case "3":
-        quarter = "Summer";
-        break;
-      default:
-        // There should never be a case where its not 1,2,3,4, right..
-        quarter = "Fall";
-    }
-    const session = quarter + " " + year;
-    gradeDist.session = session;
-  }
-
   return (
-    <OurTable
-      data={gradeData}
-      columns={columns}
-      testid="CourseGradeDistTable"
-    />
+    <div>
+      <h1>Placeholder</h1>
+      <h5>
+        Course Grade Distribution Table Placeholder (Not implemented yet!)
+      </h5>
+    </div>
   );
 }
