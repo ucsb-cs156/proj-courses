eac9efd24b2d1d8122d705cf9a7f8cdfaaa9fab5
Julia Chan <jchan11323@gmail.com>
2024-05-21 17:48:50 -0700

    added story for updateGradeInfo

--- frontend/src/stories/components/Jobs/UpdateGradeInfoForm.stories.js
+++ frontend/src/stories/components/Jobs/UpdateGradeInfoForm.stories.js
@@ -1,28 +1,9 @@
 import React from "react";
-
 import UpdateGradeInfoJobForm from "main/components/Jobs/UpdateGradeInfoForm";
-import { ucsbSubjectsFixtures } from "fixtures/ucsbSubjectsFixtures";
-import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 
 export default {
   title: "components/Jobs/UpdateGradeInfoForm",
-  component: UpdateGradeInfoJobForm,
-  parameters: {
-    mockData: [
-      {
-        url: "/api/UCSBSubjects/all",
-        method: "GET",
-        status: 200,
-        response: ucsbSubjectsFixtures.threeSubjects,
-      },
-      {
-        url: "/api/systemInfo",
-        method: "GET",
-        status: 200,
-        response: systemInfoFixtures.showingBoth,
-      },
-    ],
-  },
+  component: UpdateGradeInfoJobForm
 };
 
 const Template = (args) => {
