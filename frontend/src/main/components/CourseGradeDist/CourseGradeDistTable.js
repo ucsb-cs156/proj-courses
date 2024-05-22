d2cd01b6dda6862cc73acb5290aacde6eb41f9b8
Andrew <andrew344c@gmail.com>
2024-05-21 17:31:05 -0700

    Add placeholder course grade dist table and storybook for component

---
+++ frontend/src/main/components/CourseGradeDist/CourseGradeDistTable.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-vars
+export default function CourseGradeDistTable({ gradeData }) {
+    return <h1>Course Grade Distribution Table Placeholder (Not implemented yet!)</h1>
+}
