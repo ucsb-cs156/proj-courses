1175b45f669fce2164225a0b1b1154873f07f2f7
WinbertZhang <winbertzhang@gmail.com>
2024-05-22 17:50:50 -0700

    Revert "Merge branch 'Winbert-PSBackendDelete' into Winbert-PSFrontend"
    This reverts commit 3c0f5832ce4e83811c9a59d1c28880c5f5527110, reversing
    changes made to 49589b5d887be5146c9680e5ca65c467a50a0863.

--- frontend/src/main/components/PersonalSections/PersonalSectionsTable.js
+++ frontend/src/main/components/PersonalSections/PersonalSectionsTable.js
@@ -1,13 +1,31 @@
 import React from "react";
-import OurTable from "main/components/OurTable";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
 import {
   convertToFraction,
   formatInstructors,
   formatLocation,
   formatTime,
+  cellToAxiosParamsDelete,
+  onDeleteSuccess,
 } from "main/utils/sectionUtils.js";
+import { hasRole } from "main/utils/currentUser";
 
-export default function PersonalSectionsTable({ personalSections }) {
+export default function PersonalSectionsTable({
+  personalSections,
+  currentUser,
+  psId,
+}) {
+  const deleteMutation = useBackendMutation(
+    cellToAxiosParamsDelete,
+    { onSuccess: onDeleteSuccess },
+    [],
+  );
+
+  const deleteCallback = async (cell) => {
+    deleteMutation.mutate({ cell, psId });
+  };
+
   const columns = [
     {
       Header: "Course ID",
@@ -55,9 +73,16 @@ export default function PersonalSectionsTable({ personalSections }) {
     },
   ];
 
+  const columnsIfUser = [
+    ...columns,
+    ButtonColumn("Delete", "danger", deleteCallback, "CourseTable"),
+  ];
+
   const testid = "PersonalSectionsTable";
 
-  const columnsToDisplay = columns;
+  const columnsToDisplay = hasRole(currentUser, "ROLE_USER")
+    ? columnsIfUser
+    : columns;
 
   return (
     <OurTable
