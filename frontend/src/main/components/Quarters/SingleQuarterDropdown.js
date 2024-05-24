3c900fcd71a1bc8237841ad4d538aad764e30d51
Frank404NotFound <ye_zhong@ucsb.edu>
2024-05-23 18:09:27 -0700

    change default quarter to end qtr

--- frontend/src/main/components/Quarters/SingleQuarterDropdown.js
+++ frontend/src/main/components/Quarters/SingleQuarterDropdown.js
@@ -1,18 +1,6 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
-// controlId is used to remember the value for localStorage,
-// and for the testId, so it should be unique to at least any
-// given page where the component is used.
-
-// quarter and setQuarter should be values returned
-// by a parent component's setState
-
-// quarters is an array of objects in this format
-// [{ yyyyq :"20214", qyy: "F21"},
-//  { yyyyq :"20221", qyy: "W22"},
-//  { yyyyq :"20222", qyy: "S22"}]
-
 function SingleQuarterDropdown({
   quarter,
   quarters,
@@ -22,13 +10,15 @@ function SingleQuarterDropdown({
   label = "Quarter",
 }) {
   const localSearchQuarter = localStorage.getItem(controlId);
+  const lastIndex = quarters.length - 1;
+  const defaultQuarter = quarters[lastIndex].yyyyq;
+
   if (!localSearchQuarter) {
-    localStorage.setItem(controlId, quarters[0].yyyyq);
+    localStorage.setItem(controlId, defaultQuarter);
   }
 
   const [quarterState, setQuarterState] = useState(
-    // Stryker disable next-line all : not sure how to test/mock local storage
-    quarter.yyyq || localSearchQuarter || quarters[0].yyyyq,
+    quarter.yyyq || localSearchQuarter || defaultQuarter,
   );
 
   const handleQuarterOnChange = (event) => {
