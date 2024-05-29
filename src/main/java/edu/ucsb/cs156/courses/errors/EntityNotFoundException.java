d0119483267e0a88c9b60d777273d37fbfc0fc0c
Kelly Flippo <kellyflippo@169-231-195-85.wireless.ucsb.edu>
2024-05-29 14:54:03 -0700

    Fixing java formatting

--- src/main/java/edu/ucsb/cs156/courses/errors/EntityNotFoundException.java
+++ src/main/java/edu/ucsb/cs156/courses/errors/EntityNotFoundException.java
@@ -5,7 +5,19 @@ public class EntityNotFoundException extends RuntimeException {
     super("%s with id %s not found".formatted(entityType.getSimpleName(), id.toString()));
   }
 
-  public EntityNotFoundException(Class<?> entityType, String fieldOneName, Object fieldOneValue, String fieldTwoName, Object fieldTwoValue) {
-    super("%s with %s %s and %s %s not found".formatted(entityType.getSimpleName(), fieldOneName, fieldOneValue.toString(), fieldTwoName, fieldTwoValue.toString()));
+  public EntityNotFoundException(
+      Class<?> entityType,
+      String fieldOneName,
+      Object fieldOneValue,
+      String fieldTwoName,
+      Object fieldTwoValue) {
+    super(
+        "%s with %s %s and %s %s not found"
+            .formatted(
+                entityType.getSimpleName(),
+                fieldOneName,
+                fieldOneValue.toString(),
+                fieldTwoName,
+                fieldTwoValue.toString()));
   }
 }
