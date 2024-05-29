ca18a62254f0271eb89436d1eaf80913bdb47247
Aiden <aidenpham@ucsb.edu>
2024-05-28 17:00:44 -0700

    ap - fixed code formatting using 'npm run format'

--- frontend/src/main/components/GradeHistory/GradeHistoryTable.js
+++ frontend/src/main/components/GradeHistory/GradeHistoryTable.js
@@ -2,30 +2,31 @@ import OurTable from "../OurTable";
 import { fromFormat } from "main/utils/quarterUtilities";
 
 export default function GradeHistoryTable({ grades }) {
-    const columns = [
-        {
-            Header: "Quarter",
-            accessor: (row) => fromFormat(row.yyyyq),
-            id: "quarter",
-        },
-        {
-            Header: "Course ID",
-            accessor: "course",
-        },
-        {
-            Header: "Instructor",
-            accessor: "instructor",
-        },
-        {
-            Header: "Grade",
-            accessor: "grade",
-        },
-        {
-            Header: "Count",
-            accessor: "count",
-        },
-    ];
+  const columns = [
+    {
+      Header: "Quarter",
+      accessor: (row) => fromFormat(row.yyyyq),
+      id: "quarter",
+    },
+    {
+      Header: "Course ID",
+      accessor: "course",
+    },
+    {
+      Header: "Instructor",
+      accessor: "instructor",
+    },
+    {
+      Header: "Grade",
+      accessor: "grade",
+    },
+    {
+      Header: "Count",
+      accessor: "count",
+    },
+  ];
 
-    return (<OurTable data={grades} columns={columns} testid="GradeHistoryTable" />);
-}   
-
+  return (
+    <OurTable data={grades} columns={columns} testid="GradeHistoryTable" />
+  );
+}
