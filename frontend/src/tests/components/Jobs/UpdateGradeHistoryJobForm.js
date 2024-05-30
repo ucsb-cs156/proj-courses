d0cf2eef630d3334e17528d1b72e6d68b3f2edd0
Kevin Lavelle <kevinglavelle@gmail.com>
2024-05-30 04:59:55 -0700

    test

---
+++ frontend/src/tests/components/Jobs/UpdateGradeHistoryJobForm.js
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import UpdateGradeHistoryJobForm from "main/components/Jobs/UpdateGradeHistoryJobForm";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
+const mockedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedNavigate,
+}));
+
+const queryClient = new QueryClient();
+
+describe("UpdateGradeHistoryJobForm tests", () => {
+  const axiosMock = new AxiosMockAdapter(axios);
+
+  it("renders correctly", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UpdateCoursesJobForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(screen.getByText(/Update Grade History/)).toBeInTheDocument();
+  });
+
+  axiosMock.onGet("/api/systemInfo").reply(200, {
+    springH2ConsoleEnabled: false,
+    showSwaggerUILink: false,
+  });
+
+});
