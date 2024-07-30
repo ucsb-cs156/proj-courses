431910dc03cf5b77450a54aa11e29ddd8bb96f48
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-07-30 14:01:09 -0700

    pc - add configuration for MongoMappingContext
    This is needed because of the EnableMongoAuditing annotation in the main class
    which is needed to enable the lastUpdated field to be updated automatically

--- src/test/java/edu/ucsb/cs156/courses/testconfig/TestConfig.java
+++ src/test/java/edu/ucsb/cs156/courses/testconfig/TestConfig.java
@@ -6,6 +6,7 @@ import edu.ucsb.cs156.courses.services.GrantedAuthoritiesService;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @TestConfiguration
 @Import(SecurityConfig.class)
@@ -20,4 +21,14 @@ public class TestConfig {
   public GrantedAuthoritiesService grantedAuthoritiesService() {
     return new GrantedAuthoritiesService();
   }
+
+   /**
+   * This is needed because of the annotation EnableMongoAuditing on the main class.
+   * @returns a MongoMappingContext Bean
+   */
+  
+   @Bean
+   public MongoMappingContext mongoMappingContext() {
+     return new MongoMappingContext();
+   }
 }
