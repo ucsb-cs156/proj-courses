c688e30750516e893fa65e8d103f59631357caff
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-07-30 14:34:48 -0700

    pc - add methods to UpdateCollection that will be needed for controller

--- src/main/java/edu/ucsb/cs156/courses/collections/UpdateCollection.java
+++ src/main/java/edu/ucsb/cs156/courses/collections/UpdateCollection.java
@@ -1,9 +1,18 @@
 package edu.ucsb.cs156.courses.collections;
 
 import edu.ucsb.cs156.courses.documents.Update;
+
 import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UpdateCollection extends MongoRepository<Update, ObjectId> {}
+public interface UpdateCollection extends PagingAndSortingRepository<Update, ObjectId> {
+  Update save(Update update);
+  Page<Update> findBySubjectAreaAndQuarter(String subjectArea, String quarter, Pageable pageable);
+  Page<Update> findByQuarter(String quarter, Pageable pageable);
+  Page<Update> findBySubjectArea(String subjectArea, Pageable pageable);
+  Page<Update> findAll(Pageable pageable);
+}
