89ba04255164ffff13cd4a2a75906b59d7b788f2
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-07-30 17:12:21 -0700

    pc - add code to update Update collection when the update course data jobs are run

---
+++ src/main/java/edu/ucsb/cs156/courses/config/MongoConfig.java
@@ -0,0 +1,22 @@
+package edu.ucsb.cs156.courses.config;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories("edu.ucsb.cs156.courses.collections")
+@EnableMongoAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
+public class MongoConfig {
+
+    @Bean(name = "auditingDateTimeProvider")
+    public DateTimeProvider dateTimeProvider() {
+        return () -> Optional.of(OffsetDateTime.now());
+    }
+
+}
