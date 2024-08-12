3714519be236c381fbeb9726b774d4afcf426dc1
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-08-12 13:44:28 -0700

    pc - add javadoc to CoursesApplicationRunner

--- src/main/java/edu/ucsb/cs156/courses/CoursesApplicationRunner.java
+++ src/main/java/edu/ucsb/cs156/courses/CoursesApplicationRunner.java
@@ -8,12 +8,17 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Configuration;
 
+/** This class contains a `run` method that is called once at application startup time. */
 @Slf4j
 @Configuration
 public class CoursesApplicationRunner implements ApplicationRunner {
   @Autowired UCSBAPIQuarterService ucsbAPIQuarterService;
   @Autowired UCSBSubjectsService ucsbSubjectsService;
 
+  /**
+   * Called once at application startup time. Put code here if you want it to run once each time the
+   * Spring Boot application starts up.
+   */
   @Override
   public void run(ApplicationArguments args) throws Exception {
     log.info("CoursesApplicationRunner.run called");
