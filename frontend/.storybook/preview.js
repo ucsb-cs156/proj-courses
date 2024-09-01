c4050af4eb4c69355a8ce9c0970e7036abde7033
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-09-01 12:47:40 -0700

    pc - add back in stories/components/BasicCourseSearch

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,3 +1,30 @@
+import "../src/index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import 'react-toastify/dist/ReactToastify.css';
+
+import { initialize, mswLoader } from 'msw-storybook-addon'
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+const queryClient = new QueryClient();
+
+// Initialize MSW
+initialize()
+
+export const decorators = [
+  (Story) => (
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter>
+        <ToastContainer />
+        <Story />
+      </MemoryRouter>
+    </QueryClientProvider>
+  )
+];
+
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -8,6 +35,7 @@ const preview = {
       },
     },
   },
+  loaders: [mswLoader]
 };
 
 export default preview;
