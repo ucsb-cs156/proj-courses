acd23ff209e60fe28e93f4f372ae119c0902edce
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-09-02 14:37:14 -0700

    pc - fold in changes to CourseDescriptionIndexPage.stories.js

--- frontend/src/stories/pages/CourseDescriptions/CourseDescriptionIndexPage.stories.js
+++ frontend/src/stories/pages/CourseDescriptions/CourseDescriptionIndexPage.stories.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 import CourseDescriptionIndexPage from "main/pages/CourseDescriptions/CourseDescriptionIndexPage";
+import { ucsbSubjectsFixtures } from "fixtures/ucsbSubjectsFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { coursesFixtures } from "fixtures/courseFixtures";
+
+import { http, HttpResponse } from "msw";
 
 export default {
   title: "pages/CourseDescriptionIndexPage/CourseDescriptionIndexPage",
@@ -10,3 +15,28 @@ export default {
 const Template = () => <CourseDescriptionIndexPage />;
 
 export const Default = Template.bind({});
+Default.parameters = {
+  msw: [
+    http.get("/api/UCSBSubjects/all", () => {
+      return HttpResponse.json(ucsbSubjectsFixtures.threeSubjects, {
+        status: 200,
+      });
+    }),
+    http.get("/api/systemInfo", () => {
+      return HttpResponse.json(systemInfoFixtures.showingBoth, {
+        status: 200,
+      });
+    }),
+    http.get("/api/currentUser", () => {
+      return HttpResponse.status(403); // returns 403 when not logged in
+    }),
+    http.get("/api/public/basicsearch", () => {
+      return HttpResponse.json(
+        { classes: coursesFixtures.oneCourse },
+        {
+          status: 200,
+        },
+      );
+    }),
+  ],
+};
