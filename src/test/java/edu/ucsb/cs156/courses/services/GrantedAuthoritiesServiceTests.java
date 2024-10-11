33dc9fb5f4b72a1eb7ec312e540879ac98c97bf2
Daniel Jensen <djensen2@outlook.com>
2024-10-10 17:32:31 -0700

    correctly formatted new grantedAuthoritiesService with maven

--- src/test/java/edu/ucsb/cs156/courses/services/GrantedAuthoritiesServiceTests.java
+++ src/test/java/edu/ucsb/cs156/courses/services/GrantedAuthoritiesServiceTests.java
@@ -2,10 +2,9 @@ package edu.ucsb.cs156.courses.services;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Collection;
-
 import edu.ucsb.cs156.courses.repositories.UserRepository;
 import edu.ucsb.cs156.courses.testconfig.TestConfig;
+import java.util.Collection;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,31 +17,27 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
 @ExtendWith(SpringExtension.class)
 @EnableConfigurationProperties(value = SystemInfoServiceImpl.class)
 @Import(TestConfig.class)
 @ContextConfiguration
 class GrantedAuthoritiesServiceTests {
 
-    @MockBean
-    UserRepository userRepository;
+  @MockBean UserRepository userRepository;
 
-    @MockBean
-    ClientRegistrationRepository clientRegistrationRepository;
+  @MockBean ClientRegistrationRepository clientRegistrationRepository;
 
-    @Autowired
-    GrantedAuthoritiesService grantedAuthoritiesService;
+  @Autowired GrantedAuthoritiesService grantedAuthoritiesService;
 
-    @WithMockUser(roles = { "USER" })
-    @Test
-    void test_getGrantedAuthorities() {
-        // act
-        Collection<? extends GrantedAuthority> grantedAuthorities = grantedAuthoritiesService.getGrantedAuthorities();
+  @WithMockUser(roles = {"USER"})
+  @Test
+  void test_getGrantedAuthorities() {
+    // act
+    Collection<? extends GrantedAuthority> grantedAuthorities =
+        grantedAuthoritiesService.getGrantedAuthorities();
 
-        // assert
+    // assert
 
-        assertTrue(grantedAuthorities.size() > 0 );
-    }
-
+    assertTrue(grantedAuthorities.size() > 0);
+  }
 }
