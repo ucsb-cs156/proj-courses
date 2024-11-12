a5f16d9a80c25ef88e422e57b3a0df9c76930d7e
Phill Conrad <pconrad@cs.ucsb.edu>
2024-11-11 19:15:51 -0800

    pc - add logging of query string

--- src/main/java/edu/ucsb/cs156/courses/aop/LoggingAspect.java
+++ src/main/java/edu/ucsb/cs156/courses/aop/LoggingAspect.java
@@ -50,6 +50,9 @@ public class LoggingAspect {
                             request.getRequestURI(),
                             joinPoint.getSignature().getName(),
                             declaringTypeName));
+                if (request.getQueryString() != null) {
+                  log.info("===== Query String: %s".formatted(request.getQueryString()));
+                }
               }
             });
   }
