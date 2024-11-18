e8fa35d2e6ef74b26a7485093f117cde60812338
Eugene Lim <eugene_lim@ucsb.edu>
2024-11-17 22:13:24 -0800

    eugene-issue2

--- src/test/java/edu/ucsb/cs156/courses/jobs/UpdateCourseDataJobTests.java
+++ src/test/java/edu/ucsb/cs156/courses/jobs/UpdateCourseDataJobTests.java
@@ -96,15 +96,15 @@ public class UpdateCourseDataJobTests {
 
     // Assert
     /*
-     * Original: 
-     *         """
-                Updating courses for [CMPSC 20211]
-                Found 14 sections
-                Storing in MongoDB Collection...
-                14 new sections saved, 0 sections updated, 0 errors, last update: 2022-03-05T15:50:10
-                Saved update: Update(_id=null, subjectArea=CMPSC, quarter=20211, saved=14, updated=0, errors=0, lastUpdate=2022-03-05T15:50:10)
-                Courses for [CMPSC 20211] have been updated"""
-     */
+    * Original:
+    *         """
+               Updating courses for [CMPSC 20211]
+               Found 14 sections
+               Storing in MongoDB Collection...
+               14 new sections saved, 0 sections updated, 0 errors, last update: 2022-03-05T15:50:10
+               Saved update: Update(_id=null, subjectArea=CMPSC, quarter=20211, saved=14, updated=0, errors=0, lastUpdate=2022-03-05T15:50:10)
+               Courses for [CMPSC 20211] have been updated"""
+    */
 
     String expected =
         """
@@ -166,15 +166,15 @@ public class UpdateCourseDataJobTests {
 
     // Assert
     /*
-     * Original: 
-     * """
-                Updating courses for [MATH 20211]
-                Found 3 sections
-                Storing in MongoDB Collection...
-                2 new sections saved, 1 sections updated, 0 errors, last update: 2022-03-05T15:50:10
-                Saved update: Update(_id=null, subjectArea=MATH, quarter=20211, saved=2, updated=1, errors=0, lastUpdate=2022-03-05T15:50:10)
-                Courses for [MATH 20211] have been updated"""
-     */
+    * Original:
+    * """
+               Updating courses for [MATH 20211]
+               Found 3 sections
+               Storing in MongoDB Collection...
+               2 new sections saved, 1 sections updated, 0 errors, last update: 2022-03-05T15:50:10
+               Saved update: Update(_id=null, subjectArea=MATH, quarter=20211, saved=2, updated=1, errors=0, lastUpdate=2022-03-05T15:50:10)
+               Courses for [MATH 20211] have been updated"""
+    */
 
     String expected =
         """
@@ -229,16 +229,16 @@ public class UpdateCourseDataJobTests {
 
     // Assert
     /*
-     * Original: 
-     * """
-                Updating courses for [MATH 20211]
-                Found 1 sections
-                Storing in MongoDB Collection...
-                Error saving section: Testing Exception Handling!
-                0 new sections saved, 0 sections updated, 1 errors, last update: 2022-03-05T15:50:10
-                Saved update: Update(_id=null, subjectArea=MATH, quarter=20211, saved=0, updated=0, errors=1, lastUpdate=2022-03-05T15:50:10)
-                Courses for [MATH 20211] have been updated"""
-     */
+    * Original:
+    * """
+               Updating courses for [MATH 20211]
+               Found 1 sections
+               Storing in MongoDB Collection...
+               Error saving section: Testing Exception Handling!
+               0 new sections saved, 0 sections updated, 1 errors, last update: 2022-03-05T15:50:10
+               Saved update: Update(_id=null, subjectArea=MATH, quarter=20211, saved=0, updated=0, errors=1, lastUpdate=2022-03-05T15:50:10)
+               Courses for [MATH 20211] have been updated"""
+    */
     String expected =
         """
                 Updating courses for [MATH 20211]
@@ -297,15 +297,15 @@ public class UpdateCourseDataJobTests {
 
     // Assert
     /*
-     * Original: 
-     * """
-                Updating courses for [MATH 20211]
-                Found 1 sections
-                Storing in MongoDB Collection...
-                0 new sections saved, 1 sections updated, 0 errors, last update: 2022-03-05T15:50:10
-                Saved update: Update(_id=null, subjectArea=MATH, quarter=20211, saved=0, updated=1, errors=1, lastUpdate=2022-03-05T15:50:10)
-                Courses for [MATH 20211] have been updated"""
-     */
+    * Original:
+    * """
+               Updating courses for [MATH 20211]
+               Found 1 sections
+               Storing in MongoDB Collection...
+               0 new sections saved, 1 sections updated, 0 errors, last update: 2022-03-05T15:50:10
+               Saved update: Update(_id=null, subjectArea=MATH, quarter=20211, saved=0, updated=1, errors=1, lastUpdate=2022-03-05T15:50:10)
+               Courses for [MATH 20211] have been updated"""
+    */
 
     String expected =
         """
@@ -371,15 +371,15 @@ public class UpdateCourseDataJobTests {
 
     // Assert
     /*
-     * Original: 
-     * """
-                Updating courses for [MATH 20211]
-                Found 1 sections
-                Storing in MongoDB Collection...
-                0 new sections saved, 1 sections updated, 0 errors, last update: 2022-03-05T15:50:10
-                Saved update: Update(_id=null, subjectArea=MATH, quarter=20211, saved=0, updated=1, errors=1, lastUpdate=2022-03-05T15:50:10)
-                Courses for [MATH 20211] have been updated"""
-     */
+    * Original:
+    * """
+               Updating courses for [MATH 20211]
+               Found 1 sections
+               Storing in MongoDB Collection...
+               0 new sections saved, 1 sections updated, 0 errors, last update: 2022-03-05T15:50:10
+               Saved update: Update(_id=null, subjectArea=MATH, quarter=20211, saved=0, updated=1, errors=1, lastUpdate=2022-03-05T15:50:10)
+               Courses for [MATH 20211] have been updated"""
+    */
 
     String expected =
         """
