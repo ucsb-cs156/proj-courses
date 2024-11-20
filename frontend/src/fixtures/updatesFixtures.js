c96a1ed8106a2f349f6c118839b7c3851178ded6
Edwin Medrano <eduino@100-64-48-56.wireless.ucsb.edu>
2024-11-20 13:40:52 -0800

    EMV - met all expectations for UpdatesTable, all tests are passing and storybook is up

---
+++ frontend/src/fixtures/updatesFixtures.js
@@ -0,0 +1,56 @@
+const updatesFixtures = {
+  oneUpdate: {
+    _id: {
+      timestamp: 1731206250,
+      date: "2024-11-10T02:37:30.000+00:00",
+    },
+    subjectArea: "GRAD W",
+    quarter: "20244",
+    saved: 0,
+    updated: 0,
+    errors: 0,
+    lastUpdate: "2024-11-10T02:37:30.677",
+  },
+  threeUpdates: [
+    {
+      _id: {
+        timestamp: 1731206250,
+        date: "2024-11-10T02:37:30.000+00:00",
+      },
+      subjectArea: "GRAD W",
+      quarter: "20244",
+      saved: 0,
+      updated: 0,
+      errors: 0,
+      lastUpdate: "2024-11-10T02:37:30.677",
+    },
+
+    {
+      _id: {
+        timestamp: 1731206250,
+        date: "2024-11-10T02:37:30.000+00:00",
+      },
+      subjectArea: "IQB",
+      quarter: "20244",
+      saved: 11,
+      updated: 36,
+      errors: 0,
+      lastUpdate: "2024-11-10T02:37:30.467",
+    },
+
+    {
+      _id: {
+        timestamp: 1731206246,
+        date: "2024-11-10T02:37:26.000+00:00",
+      },
+      subjectArea: "W&L  CSW",
+      quarter: "20244",
+      saved: 0,
+      updated: 0,
+      errors: 0,
+      lastUpdate: "2024-11-10T02:37:26.354",
+    },
+  ],
+};
+
+export { updatesFixtures };
