997dd7b146361426d4533634caccbd6ad1ad0e44
Chloe <chloeweng@ucsb.edu>
2024-11-20 15:48:03 -0800

    update files

---
+++ frontend/src/fixtures/updateFixtures.js
@@ -0,0 +1,44 @@
+const updateFixtures = {
+    oneUpdate: [
+      {
+        subjectArea: "ECON",
+        quarter: "W23",
+        saved: 0,
+        updated: 0,
+        errors: 0,
+        lastUpdate: "2024-11-10T02:37:30.677",
+      },
+    ],
+  
+    threeUpdates: [
+      {
+        subjectArea: "EARTH",
+        quarter: "W23",
+        saved: 0,
+        updated: 0,
+        errors: 0,
+        lastUpdate: "2022-10-21T11:39:10",
+      },
+  
+      {
+        subjectArea: "CMPSC",
+        quarter: "W23",
+        saved: 1,
+        updated: 3,
+        errors: 0,
+        lastUpdate: "2022-01-02T12:00:00",
+      },
+  
+      {
+        subjectArea: "PSTAT",
+        quarter: "W23",
+        saved: 3,
+        updated: 8,
+        errors: 2,
+        lastUpdate: "2024-02-10T06:17:00.331",
+      },
+    ],
+  };
+  
+  export { updateFixtures };
+  
---
+++ frontend/src/stories/components/Updates/UpdateTable.stories.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import UpdateTable from "main/components/Updates/UpdateTable";
+import {
+  oneUpdate,
+  threeUpdates,
+} from "fixtures/updateFixtures";
+
+export default {
+  title: "components/Updates/UpdateTable",
+  component: UpdateTable,
+};
+
+const Template = (args) => {
+  return <UpdateTable {...args} />;
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+  updates: [],
+};
+
+export const OneUpdate = Template.bind({});
+
+OneUpdate.args = {
+  updates: oneUpdate,
+};
+
+export const ThreeUpdates = Template.bind({});
+
+ThreeUpdates.args = {
+    updates: threeUpdates,
+  };
