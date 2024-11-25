cb4907d6d0877228313b7d8013e3835574542ab2
Junhyung Yoon <junhyungyoon@169-231-200-144.wireless.ucsb.edu>
2024-11-24 23:46:33 -0800

    removed comments

--- src/test/java/edu/ucsb/cs156/courses/controllers/UCSBAPIQuarterControllerTests.java
+++ src/test/java/edu/ucsb/cs156/courses/controllers/UCSBAPIQuarterControllerTests.java
@@ -130,21 +130,4 @@ public class UCSBAPIQuarterControllerTests extends ControllerTestCase {
         objectMapper.readValue(
             response.getResponse().getContentAsString(), new TypeReference<List<String>>() {}));
   }
-
-  //   @Test
-  //   public void test_getActiveQuarterList_S20_F19_1() throws Exception {
-  //     ArrayList<UCSBAPIQuarter> expected = new ArrayList<UCSBAPIQuarter>();
-  //     expected.add(new Quarter("S20"));
-  //     assertEquals(expected, Quarter.quarterList("20202", "20202"));
-  //   }
-
-  //   @Test
-  //   public void test_getActiveQuarterList_F19_S20() throws Exception {
-  //     List<Quarter> expected = new ArrayList<Quarter>();
-  //     expected.add(new Quarter("F19"));
-  //     expected.add(new Quarter("W20"));
-  //     expected.add(new Quarter("S20"));
-
-  //     assertEquals(expected, Quarter.quarterList("20194", "20202"));
-  //   }
 }
