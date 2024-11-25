7888d70224fe57937de518c1eb8c7469dc6afb37
Phill Conrad <pconrad@cs.ucsb.edu>
2024-11-24 22:18:34 -0800

    Update src/main/java/edu/ucsb/cs156/courses/entities/FinalsEntity.java
    Adding import for `LastModifiedDate;`

--- src/main/java/edu/ucsb/cs156/courses/entities/FinalsEntity.java
+++ src/main/java/edu/ucsb/cs156/courses/entities/FinalsEntity.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Data
 @Builder
