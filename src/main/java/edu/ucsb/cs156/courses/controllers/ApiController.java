cce9700b7209b20efbc2d7522e1eb004635723ce
Cameron Byrne <cameron.byrne@outlook.com>
2024-11-26 14:07:15 -0800

    ran mvn format to fix mvn build issue (works locally)

--- src/main/java/edu/ucsb/cs156/courses/controllers/ApiController.java
+++ src/main/java/edu/ucsb/cs156/courses/controllers/ApiController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
 // testing how creating a draft pull request works! cameron
 @Slf4j
 public abstract class ApiController {
