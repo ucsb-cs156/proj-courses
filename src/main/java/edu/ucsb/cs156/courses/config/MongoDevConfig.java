0c815b307b747825b27be8ba9ed68c13d3313eb0
Daniel Jensen <djensen2@outlook.com>
2024-12-14 11:41:05 -0800

    dj - ran code formatting

--- src/main/java/edu/ucsb/cs156/courses/config/MongoDevConfig.java
+++ src/main/java/edu/ucsb/cs156/courses/config/MongoDevConfig.java
@@ -1,8 +1,8 @@
 package edu.ucsb.cs156.courses.config;
 
+import de.flapdoodle.embed.mongo.spring.autoconfigure.EmbeddedMongoAutoConfiguration;
 import java.time.OffsetDateTime;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -12,21 +12,18 @@ import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import de.flapdoodle.embed.mongo.spring.autoconfigure.EmbeddedMongoAutoConfiguration;
 
 @Profile("development")
 @Configuration
 @EnableAutoConfiguration
 @EnableMongoRepositories("edu.ucsb.cs156.courses.collections")
 @EnableMongoAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
-public class MongoDevConfig extends EmbeddedMongoAutoConfiguration{
+public class MongoDevConfig extends EmbeddedMongoAutoConfiguration {
 
-    @Bean(name = "auditingDateTimeProvider")
-    public DateTimeProvider dateTimeProvider() {
-        return () -> Optional.of(OffsetDateTime.now());
-    }
+  @Bean(name = "auditingDateTimeProvider")
+  public DateTimeProvider dateTimeProvider() {
+    return () -> Optional.of(OffsetDateTime.now());
+  }
 
-    public void mongoInstance(@Autowired MongoTemplate mongoTemplate) {
-
-    }
+  public void mongoInstance(@Autowired MongoTemplate mongoTemplate) {}
 }
