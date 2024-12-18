a32c0839e6f147b50e24cf4e36454d72550372c1
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-12-17 18:47:49 -0800

    pc - update UCSBSubjectsService to exclude bad data returned by endpoint
    For unknown reasons, the API was sometimes returning a record containing
    the field names.  This code filters that out.

--- src/test/java/edu/ucsb/cs156/courses/services/UCSBSubjectsServiceTests.java
+++ src/test/java/edu/ucsb/cs156/courses/services/UCSBSubjectsServiceTests.java
@@ -1,6 +1,6 @@
 package edu.ucsb.cs156.courses.services;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
@@ -30,38 +30,46 @@ class UCSBSubjectsServiceTests {
   @Value("${app.ucsb.api.consumer_key}")
   private String apiKey;
 
-  private static final String SUBJECTCODE = "SUBJECTCODE";
-  private static final String SUBJECTTRANSLATION = "SUBJECTTRANSLATION";
-  private static final String DEPTCODE = "DEPTCODE";
-  private static final String COLLEGECODE = "COLLEGECODE";
-  private static final String RELATEDDEPTCODE = "RELATEDDEPTCODE";
+  private static final String SUBJECTCODE = "ANTH";
+  private static final String SUBJECTTRANSLATION = "Anthropology";
+  private static final String DEPTCODE = "ANTH";
+  private static final String COLLEGECODE = "L&S";
+  private static final String RELATEDDEPTCODE = "NONE";
   private static final Boolean INACTIVE = false;
 
-  @Test
-  void get_returns_a_list_of_subjects() throws Exception {
-
-    String expectedURL = UCSBSubjectsService.ENDPOINT;
-
-    String expectedResult =
-        String.format(
-            """
-            [
+  private String expectedResult =
+      String.format(
+          """
+          [
             {
               \"subjectCode\": \"%s\",
               \"subjectTranslation\":\"%s\",
               \"deptCode\": \"%s\",
               \"collegeCode\": \"%s\",
               \"relatedDeptCode\": \"%s\",
-              \"inactive\": \"%s\"
+              \"inactive\": %s
+            },
+            {
+              \"subjectCode\": \"SUBJECTCODE\",
+              \"subjectTranslation\":\"SUBJECTTRANSLATION\",
+              \"deptCode\": \"DEPTCODE\",
+              \"collegeCode\": \"COLLEGECODE\",
+              \"relatedDeptCode\": \"RELATEDDEPTCODE\",
+              \"inactive\": false
             }
-            ]
-            """,
-            SUBJECTCODE,
-            SUBJECTTRANSLATION,
-            DEPTCODE,
-            COLLEGECODE,
-            RELATEDDEPTCODE,
-            INACTIVE.toString());
+          ]
+          """,
+          SUBJECTCODE,
+          SUBJECTTRANSLATION,
+          DEPTCODE,
+          COLLEGECODE,
+          RELATEDDEPTCODE,
+          INACTIVE.toString());
+
+  @Test
+  void get_returns_a_list_of_subjects() throws Exception {
+
+    String expectedURL = UCSBSubjectsService.ENDPOINT;
 
     UCSBSubject expectedSubject =
         UCSBSubject.builder()
@@ -83,34 +91,13 @@ class UCSBSubjectsServiceTests {
     List<UCSBSubject> actualResult = ucsbSubjectsService.get();
     List<UCSBSubject> expectedList = new ArrayList<>();
     expectedList.addAll(Arrays.asList(expectedSubject));
-    assertEquals(expectedList, actualResult);
+    assertIterableEquals(expectedList, actualResult);
   }
 
   @Test
   void test_loadAllSubjects() throws Exception {
 
     String expectedURL = UCSBSubjectsService.ENDPOINT;
-
-    String expectedResult =
-        String.format(
-            """
-                [
-                  {
-                    \"subjectCode\": \"%s\",
-                    \"subjectTranslation\":\"%s\",
-                    \"deptCode\": \"%s\",
-                    \"collegeCode\": \"%s\",
-                    \"relatedDeptCode\": \"%s\",
-                    \"inactive\": \"%s\"
-                  }
-                ]
-            """,
-            SUBJECTCODE,
-            SUBJECTTRANSLATION,
-            DEPTCODE,
-            COLLEGECODE,
-            RELATEDDEPTCODE,
-            INACTIVE.toString());
 
     UCSBSubject expectedSubject =
         UCSBSubject.builder()
@@ -132,6 +119,6 @@ class UCSBSubjectsServiceTests {
     List<UCSBSubject> actualResult = ucsbSubjectsService.loadAllSubjects();
     List<UCSBSubject> expectedList = new ArrayList<>();
     expectedList.addAll(Arrays.asList(expectedSubject));
-    assertEquals(expectedList, actualResult);
+    assertIterableEquals(expectedList, actualResult);
   }
 }
