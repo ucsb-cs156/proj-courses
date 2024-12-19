e4d300a99b39ccafb4ea7b9f11c82ea8ef04f4ac
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-12-19 10:52:54 -0800

    pc - add pagination to /api/updates endpoint

--- src/test/java/edu/ucsb/cs156/courses/controllers/UpdateControllerTests.java
+++ src/test/java/edu/ucsb/cs156/courses/controllers/UpdateControllerTests.java
@@ -11,6 +11,7 @@ import edu.ucsb.cs156.courses.collections.UpdateCollection;
 import edu.ucsb.cs156.courses.documents.Update;
 import edu.ucsb.cs156.courses.testconfig.TestConfig;
 import java.util.ArrayList;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
@@ -39,8 +40,13 @@ public class UpdateControllerTests extends ControllerTestCase {
   ArrayList<Update> emptyArray = new ArrayList<Update>();
   PageRequest pageRequest_0_10_DESC_lastUpdate =
       PageRequest.of(0, 10, Direction.DESC, "lastUpdate");
+  PageRequest pageRequest_0_10_ASC_subjectArea =
+      PageRequest.of(0, 10, Direction.ASC, "subjectArea");
+
   private final Page<Update> emptyPage_0_10_DESC_lastUpdate =
       new PageImpl<Update>(emptyArray, pageRequest_0_10_DESC_lastUpdate, 0);
+  private final Page<Update> emptyPage_0_10_ASC_subjectArea =
+      new PageImpl<Update>(emptyArray, pageRequest_0_10_ASC_subjectArea, 0);
 
   @Test
   @WithMockUser(roles = {"ADMIN"})
@@ -54,7 +60,9 @@ public class UpdateControllerTests extends ControllerTestCase {
     // act
     MvcResult response =
         mockMvc
-            .perform(get("/api/updates?subjectArea=ALL&quarter=ALL&page=0&pageSize=10"))
+            .perform(
+                get(
+                    "/api/updates?subjectArea=ALL&quarter=ALL&page=0&pageSize=10&sortField=lastUpdate&sortDirection=DESC"))
             .andExpect(status().isOk())
             .andReturn();
 
@@ -76,7 +84,9 @@ public class UpdateControllerTests extends ControllerTestCase {
     // act
     MvcResult response =
         mockMvc
-            .perform(get("/api/updates?subjectArea=CMPSC&quarter=ALL&page=0&pageSize=10"))
+            .perform(
+                get(
+                    "/api/updates?subjectArea=CMPSC&quarter=ALL&page=0&pageSize=10&sortField=lastUpdate&sortDirection=DESC"))
             .andExpect(status().isOk())
             .andReturn();
 
@@ -98,7 +108,9 @@ public class UpdateControllerTests extends ControllerTestCase {
     // act
     MvcResult response =
         mockMvc
-            .perform(get("/api/updates?subjectArea=ALL&quarter=20221&page=0&pageSize=10"))
+            .perform(
+                get(
+                    "/api/updates?subjectArea=ALL&quarter=20221&page=0&pageSize=10&sortField=lastUpdate&sortDirection=DESC"))
             .andExpect(status().isOk())
             .andReturn();
 
@@ -115,18 +127,72 @@ public class UpdateControllerTests extends ControllerTestCase {
     // arrange
 
     when(updateCollection.findBySubjectAreaAndQuarter(
-            "CMPSC", "20221", pageRequest_0_10_DESC_lastUpdate))
-        .thenReturn(emptyPage_0_10_DESC_lastUpdate);
+            "CMPSC", "20221", pageRequest_0_10_ASC_subjectArea))
+        .thenReturn(emptyPage_0_10_ASC_subjectArea);
 
     // act
     MvcResult response =
         mockMvc
-            .perform(get("/api/updates?subjectArea=CMPSC&quarter=20221&page=0&pageSize=10"))
+            .perform(
+                get(
+                    "/api/updates?subjectArea=CMPSC&quarter=20221&page=0&pageSize=10&sortField=subjectArea&sortDirection=ASC"))
             .andExpect(status().isOk())
             .andReturn();
 
     // assert
-    String expectedResponseAsJson = objectMapper.writeValueAsString(emptyPage_0_10_DESC_lastUpdate);
+    String expectedResponseAsJson = objectMapper.writeValueAsString(emptyPage_0_10_ASC_subjectArea);
+    String actualResponse = response.getResponse().getContentAsString();
+    assertEquals(expectedResponseAsJson, actualResponse);
+  }
+
+  @Test
+  @WithMockUser(roles = {"ADMIN"})
+  public void when_sortField_is_invalid_throws_exception() throws Exception {
+
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(
+                get(
+                    "/api/updates?subjectArea=CMPSC&quarter=20221&page=0&pageSize=10&sortField=invalid&sortDirection=DESC"))
+            .andExpect(status().isBadRequest())
+            .andReturn();
+
+    // assert
+    Map<String, String> expectedResponse =
+        Map.of(
+            "message",
+            "invalid is not a valid sort field.  Valid values are [subjectArea, quarter, lastUpdate]",
+            "type",
+            "IllegalArgumentException");
+
+    String expectedResponseAsJson = objectMapper.writeValueAsString(expectedResponse);
+    String actualResponse = response.getResponse().getContentAsString();
+    assertEquals(expectedResponseAsJson, actualResponse);
+  }
+
+  @Test
+  @WithMockUser(roles = {"ADMIN"})
+  public void when_sortDirection_is_invalid_throws_exception() throws Exception {
+
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(
+                get(
+                    "/api/updates?subjectArea=CMPSC&quarter=20221&page=0&pageSize=10&sortField=subjectArea&sortDirection=INVALID"))
+            .andExpect(status().isBadRequest())
+            .andReturn();
+
+    // assert
+    Map<String, String> expectedResponse =
+        Map.of(
+            "message",
+            "INVALID is not a valid sort direction.  Valid values are [ASC, DESC]",
+            "type",
+            "IllegalArgumentException");
+
+    String expectedResponseAsJson = objectMapper.writeValueAsString(expectedResponse);
     String actualResponse = response.getResponse().getContentAsString();
     assertEquals(expectedResponseAsJson, actualResponse);
   }
