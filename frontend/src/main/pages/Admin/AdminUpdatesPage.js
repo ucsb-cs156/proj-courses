39993d7a71040e93e37f94beb486a893db565eee
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-12-19 17:15:44 -0800

    pc - mutation coverage for AdminUpdatesPage

--- frontend/src/main/pages/Admin/AdminUpdatesPage.js
+++ frontend/src/main/pages/Admin/AdminUpdatesPage.js
@@ -2,30 +2,31 @@ import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import UpdatesSearchForm from "main/components/Updates/UpdatesSearchForm";
 import UpdatesTable from "main/components/Updates/UpdatesTable";
 import { useState } from "react";
+import useLocalStorage from "main/utils/useLocalStorage";
 import { useBackend } from "main/utils/useBackend";
 import OurPagination from "main/components/Utils/OurPagination";
 
 export default function AdminUpdatesPage() {
-  const testId = "AdminUpdatesPage";
-
   const [selectedPage, setSelectedPage] = useState(1);
   const refreshJobsIntervalMilliseconds = 500;
 
-  const localSubject = localStorage.getItem("UpdatesSearch.SubjectArea");
-  const localQuarter = localStorage.getItem("UpdatesSearch.Quarter");
-  const localSortField = localStorage.getItem("UpdatesSearch.SortField");
-  const localSortDirection = localStorage.getItem(
+  const [quarter, setQuarter] = useLocalStorage("UpdatesSearch.Quarter", "ALL");
+  const [subject, setSubject] = useLocalStorage(
+    "UpdatesSearch.SubjectArea",
+    "ALL",
+  );
+  const [sortField, setSortField] = useLocalStorage(
+    "UpdatesSearch.SortField",
+    "subjectArea",
+  );
+  const [sortDirection, setSortDirection] = useLocalStorage(
     "UpdatesSearch.SortDirection",
+    "ASC",
   );
-  const localPageSize = localStorage.getItem("UpdatesSearch.PageSize");
-
-  const [quarter, setQuarter] = useState(localQuarter || "ALL");
-  const [subject, setSubject] = useState(localSubject || "ALL");
-  const [sortField, setSortField] = useState(localSortField || "subjectArea");
-  const [sortDirection, setSortDirection] = useState(
-    localSortDirection || "ASC",
+  const [pageSize, setPageSize] = useLocalStorage(
+    "UpdatesSearch.PageSize",
+    "10",
   );
-  const [pageSize, setPageSize] = useState(localPageSize || "10");
 
   // Stryker disable all
   const { data: page } = useBackend(
@@ -60,7 +61,6 @@ export default function AdminUpdatesPage() {
       <OurPagination
         updateActivePage={setSelectedPage}
         totalPages={page.totalPages}
-        testId={testId}
       />
       <UpdatesTable updates={page.content} />
     </BasicLayout>
