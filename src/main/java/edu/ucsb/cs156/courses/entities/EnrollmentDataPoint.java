e972dbb5846953866a561021b744daab4cd34799
Phillip Conrad <pconrad@cs.ucsb.edu>
2025-02-13 18:59:01 -0800

    pc - fix formatting

--- src/main/java/edu/ucsb/cs156/courses/entities/EnrollmentDataPoint.java
+++ src/main/java/edu/ucsb/cs156/courses/entities/EnrollmentDataPoint.java
@@ -1,12 +1,9 @@
 package edu.ucsb.cs156.courses.entities;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/edu/ucsb/cs156/courses/repositories/EnrollmentDataPointRepository.java
+++ src/main/java/edu/ucsb/cs156/courses/repositories/EnrollmentDataPointRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnrollmentDataPointRepository extends CrudRepository<EnrollmentDataPoint, Long> {
- 
-}
+public interface EnrollmentDataPointRepository extends CrudRepository<EnrollmentDataPoint, Long> {}
