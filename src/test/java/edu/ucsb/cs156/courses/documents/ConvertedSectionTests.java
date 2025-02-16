05064fe84353143301db37fa2d71d31af52c12e3
Phillip Conrad <pconrad@cs.ucsb.edu>
2025-02-16 11:04:23 -0800

    pc add tests for code to get EnrollmentDataPoint from a converted section

--- src/test/java/edu/ucsb/cs156/courses/documents/ConvertedSectionTests.java
+++ src/test/java/edu/ucsb/cs156/courses/documents/ConvertedSectionTests.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.ucsb.cs156.courses.entities.EnrollmentDataPoint;
+
 import java.util.List;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
@@ -32,5 +35,19 @@ public class ConvertedSectionTests {
     cs1.set_id(new ObjectId());
     ConvertedSection cs2 = (ConvertedSection) cs1.clone();
     assertEquals(cs1, cs2);
+  }
+
+  @Test
+  public void test_EnrollmentDataPoint() throws JsonProcessingException {
+    List<ConvertedSection> cs =
+        mapper.readValue(
+            CoursePageFixtures.CONVERTED_SECTIONS_JSON_MATH3B,
+            new TypeReference<List<ConvertedSection>>() {});
+
+    ConvertedSection cs1 = cs.get(0);
+    EnrollmentDataPoint edp = cs1.getEnrollmentDataPoint();
+    assertEquals("20211", edp.getYyyyq());
+    assertEquals("10001", edp.getEnrollCd());
+    assertEquals(10, edp.getEnrollment());
   }
 }
