de6c0b57067c140538db9bc5be08d4f665a5f41a
Daniel Jensen <djensen2@outlook.com>
2025-04-28 15:07:43 -0700

    dj - switched delimiter for -d as --delimiter is not supported

--- startup.sh
+++ startup.sh
@@ -3,13 +3,9 @@ if [ -n "$DOKKU_POSTGRES_AQUA_URL" ]; then
 DATABASE_URL="$DOKKU_POSTGRES_AQUA_URL"
 fi
 
-export JDBC_DATABASE_PASSWORD=$(echo "$DATABASE_URL" | cut --delimiter=: -f3 | cut --delimiter=\@ -f1)
+export JDBC_DATABASE_PASSWORD=$(echo "$DATABASE_URL" | cut -d : -f3 | cut -d \@ -f1)
 
-echo "$JDBC_DATABASE_PASSWORD"
-
-export JDBC_DATABASE_URL=jdbc:postgresql://$(echo "$DATABASE_URL" | cut --delimiter=\@ -f2)
-
-echo "$JDBC_DATABASE_URL"
+export JDBC_DATABASE_URL=jdbc:postgresql://$(echo "$DATABASE_URL" | cut -d \@ -f2)
 
 export JDBC_DATABASE_USERNAME=postgres
 
