033689035d68e2a2f103ed3a7328eda08aaaf446
Eyob Teshome <teshome@ucsb.edu>
2025-05-15 15:33:27 -0700

    eyobte - Added Endpoint for GE in Curriculum Serviceremoved ge entities and repositories as they were unnecesary. :

--- src/main/java/edu/ucsb/cs156/courses/repositories/UCSBGERepository.java
+++
@@ -1,8 +0,0 @@
-package edu.ucsb.cs156.courses.repositories;
-
-import edu.ucsb.cs156.courses.entities.UCSBGE;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UCSBGERepository extends CrudRepository<UCSBGE, String> {}
