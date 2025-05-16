a6c91e8a2eba80ec8bf01f4ffd9f48faea19ff9d
Mujia Chen <mujia@ucsb.edu>
2025-05-16 13:58:17 -0700

    mc - add placeholder page for GE search

---
+++ frontend/src/main/pages/GeneralEducation/Search/GeneralEducationSearchPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function GeneralEducationSearchPage() {
+  return (
+    <div className="container mt-3">
+      <h1>GE Search coming soon!</h1>
+    </div>
+  );
+}
