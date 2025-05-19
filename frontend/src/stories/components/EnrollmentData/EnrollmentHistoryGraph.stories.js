005aa95aed616262dda75a33cc728cfd5eb99919
Isaac Landry <ilandry@ucsb.edu>
2025-05-19 11:34:19 -0700

    il - WIP. Need to get the storybook to work with three enrollment data points.

--- frontend/src/stories/components/EnrollmentData/EnrollmentHistoryGraph.stories.js
+++ frontend/src/stories/components/EnrollmentData/EnrollmentHistoryGraph.stories.js
@@ -3,9 +3,10 @@ import React from "react";
 import EnrollmentHistoryGraphs from "main/components/EnrollmentData/EnrollmentHistoryGraph";
 import {
   oneQuarterCourse,
-  twoQuarterCourse,
-  fullCourse,
+  // twoQuarterCourse,
+  // fullCourse,
 } from "fixtures/gradeHistoryFixtures";
+import { oneEnrollmentDataPoint, _threeEnrollmentDataPoints } from "fixtures/enrollmentDataPointFixtures";
 
 export default {
   title: "components/EnrollmentData/EnrollmentDataTable",
@@ -21,17 +22,27 @@ Empty.args = {
   gradeHistory: [],
 };
 
+export const OneEnrollmentData = Template.bind({});
+OneEnrollmentData.args = {
+  enrollmentHistory: oneEnrollmentDataPoint,
+};
+
+// export const ThreeEnrollmentData = Template.bing({});
+// ThreeEnrollmentData.args = {
+//   enrollmentHistory: threeEnrollmentDataPoints,
+// };
+
 export const OneQuarterCourse = Template.bind({});
 OneQuarterCourse.args = {
   gradeHistory: oneQuarterCourse,
 };
 
-export const TwoQuarterCourse = Template.bind({});
-TwoQuarterCourse.args = {
-  gradeHistory: twoQuarterCourse,
-};
+// export const TwoQuarterCourse = Template.bind({});
+// TwoQuarterCourse.args = {
+//   gradeHistory: twoQuarterCourse,
+// };
 
-export const FullCourse = Template.bind({});
-FullCourse.args = {
-  gradeHistory: fullCourse,
-};
+// export const FullCourse = Template.bind({});
+// FullCourse.args = {
+//   gradeHistory: fullCourse,
+// };
