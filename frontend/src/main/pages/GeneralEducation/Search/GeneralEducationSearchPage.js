da5c3aaf74151ba404d366b917ac0530fc345263
Mujia Chen <mujia@ucsb.edu>
2025-05-18 18:54:43 -0700

    mc - wrapped in <BasicLayout> so that they have the navigation header, and footer

--- frontend/src/main/pages/GeneralEducation/Search/GeneralEducationSearchPage.js
+++ frontend/src/main/pages/GeneralEducation/Search/GeneralEducationSearchPage.js
@@ -1,9 +1,12 @@
 import React from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
 export default function GeneralEducationSearchPage() {
   return (
     <BasicLayout>
-      <h1>GE Search coming soon!</h1>
-        </BasicLayout>
+      <div className="container mt-3">
+        <h1>GE Search coming soon!</h1>
+      </div>
+    </BasicLayout>
   );
 }
