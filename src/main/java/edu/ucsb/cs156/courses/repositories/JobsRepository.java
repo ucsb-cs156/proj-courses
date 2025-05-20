691e04978afa092a884d364ec230d258baf29da1
Xinyan Zhao <xinyan_zhao@ucsb.edu>
2025-05-19 22:36:32 -0700

    xz - edited the format and passed jacoco

--- src/main/java/edu/ucsb/cs156/courses/repositories/JobsRepository.java
+++ src/main/java/edu/ucsb/cs156/courses/repositories/JobsRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-
 @Repository
 public interface JobsRepository extends CrudRepository<Job, Long> {
     Page<Job> findAll(Pageable pageable);
