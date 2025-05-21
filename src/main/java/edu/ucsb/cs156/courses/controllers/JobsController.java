3d55bbd144496409bdba4258fd7b44f4c15415dd
Awin Zhang <awin@ucsb.edu>
2025-05-20 20:37:10 -0700

    fixed format

--- src/main/java/edu/ucsb/cs156/courses/controllers/JobsController.java
+++ src/main/java/edu/ucsb/cs156/courses/controllers/JobsController.java
@@ -239,8 +239,7 @@ public class JobsController extends ApiController {
           @RequestParam(defaultValue = "DESC")
           String sortDirection) {
 
-    List<String> allowedSortFields =
-        Arrays.asList("status", "createdAt", "updatedAt");
+    List<String> allowedSortFields = Arrays.asList("status", "createdAt", "updatedAt");
 
     if (!allowedSortFields.contains(sortField)) {
       throw new IllegalArgumentException(
