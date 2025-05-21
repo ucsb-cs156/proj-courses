4219855d239559f00bec0118ad59afc9d1a20a50
Alec Sekimoto <alecsekimoto@ucsb.edu>
2025-05-21 15:35:16 -0700

    as - fixed bugs in SingleGEDropdown and added tests

---
+++ frontend/src/fixtures/GEAreaFixtures.js
@@ -0,0 +1,29 @@
+export const oneGEArea = [
+  {
+    requirementCode: "A1",
+  },
+];
+
+export const threeGEAreas = [
+  {
+    requirementCode: "A1",
+  },
+  {
+    requirementCode: "B",
+  },
+  {
+    requirementCode: "C",
+  },
+];
+
+export const outOfOrderGEAreas = [
+  {
+    requirementCode: "C",
+  },
+  {
+    requirementCode: "B",
+  },
+  {
+    requirementCode: "A1",
+  },
+];
