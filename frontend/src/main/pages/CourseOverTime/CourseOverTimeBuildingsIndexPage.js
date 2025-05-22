bac16d2495bd33f237ea09c270a595a941b0ca09
Oscar Luna <oscar_luna@ucsb.edu>
2025-05-21 20:38:57 -0700

    tab

--- frontend/src/main/pages/CourseOverTime/CourseOverTimeBuildingsIndexPage.js
+++ frontend/src/main/pages/CourseOverTime/CourseOverTimeBuildingsIndexPage.js
@@ -26,7 +26,7 @@ export default function CourseOverTimeBuildingsIndexPage() {
       buildingCode: query.buildingCode,
     },
   });
-  
+
   const onSuccess = (buildings) => {
     setCourseJSON(buildings);
   };
