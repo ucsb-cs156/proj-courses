80e015a27464c7dd631523c1fb8ad4d82b14f5be
Phill Conrad <pconrad@cs.ucsb.edu>
2025-07-26 18:48:51 -0700

    pc - WIP on updating for TanStack Table
    We now have a proof of concept that we can use to make the new SectionsTableBase
    
    The next step is to understand how the various other tables rely on it,
    and refactor them to use the new SectionsTableBase

--- frontend/src/main/components/Common/PrimaryTable.js
+++ frontend/src/main/components/Common/PrimaryTable.js
@@ -1,129 +1,17 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
+import { Table } from "react-bootstrap"
 import {
   useReactTable,
   getCoreRowModel,
   getExpandedRowModel,
   flexRender,
 } from '@tanstack/react-table';
-import primaryFixtures from 'fixtures/primaryFixtures';
 
-
-function formatInstructorsToString(instructorsArray) {
-  if (!Array.isArray(instructorsArray) || instructorsArray.length === 0) {
-    return ""; 
-  }
-  const instructorNames = instructorsArray.map((instructorObj) => instructorObj.instructor);
-  return instructorNames.join(", ");
-}
-
-function PrimaryTable({testId = "PrimaryTable" }) {
-
-  const data = primaryFixtures.f24_math_lowerDiv;
-
-  const getSectionField = (row, key) => ('primary' in row.original) ? row.original.primary[key] : row.original[key]
-  const getSectionTimeLocationField = (row, key) => ('primary' in row.original) ? row.original.primary.timeLocations?.[0][key] : row.original.timeLocations?.[0][key]
-  const getSectionInstructors = (row) => {
-    const instructors = 'primary' in row.original ? row.original.primary.instructors : row.original.instructors;
-    return formatInstructorsToString(instructors)
-  }
-
-  const columns = React.useMemo(
-    () => [
-      {
-        id: 'expander', // Unique ID for the expander column
-        header: ({ table }) => (
-          <button
-            {...{
-              onClick: table.getToggleAllRowsExpandedHandler(),
-            }}
-          >
-            {table.getIsAllRowsExpanded() ? '-' : '+'}
-          </button>
-        ),
-        cell: ({ row }) =>
-          row.getCanExpand() ? (
-            <button
-              {...{
-                onClick: row.getToggleExpandedHandler(),
-                style: { cursor: 'pointer' },
-              }}
-            >
-              {row.getIsExpanded() ? '-' : '+'}
-            </button>
-          ) : (
-            '🔵' // Or null, or an empty span for rows that can't expand
-          ),
-        // This is important for indenting sub-rows
-        // We'll apply this style in the render, but you can define it here too
-        // For sub-rows, you might want to adjust cell content for clarity
-      },
-      {
-        accessorKey: 'courseId',
-        header: 'Course ID',
-        cell: ({ row, getValue }) => (
-          <div style={{ paddingLeft: `${row.depth * 2}rem` }}>
-            {getValue()}
-          </div>
-        ),
-      },
-      {
-        accessorKey: 'title',
-        header: 'Title',
-      },
-      {
-        accessorKey: 'enrollCode',
-        header: 'Enroll Code',
-        cell: ({row}) => getSectionField(row, "enrollCode")
-      },
-      {
-        accessorKey: 'section',
-        header: 'Section',
-        cell: ({row}) => getSectionField(row, 'section')
-      },
-      {
-        accessorKey: 'enrolledTotal',
-        header: 'Enrolled',
-        cell: ({row}) => getSectionField(row, 'enrolledTotal')
-      },
-      {
-        accessorKey: 'maxEnroll',
-        header: 'Max Enroll',
-        cell: ({row}) => getSectionField(row, 'maxEnroll')
-      },
-      {
-        id: 'days',
-        header: 'Days',
-        cell: ({row}) => getSectionTimeLocationField(row, 'days')
-      },
-      {
-        id: 'beginTime',
-        header: 'Begin Time',
-        cell: ({row}) => getSectionTimeLocationField(row, 'beginTime')
-
-      },
-      {
-        id: 'endTime',
-        header: 'End Time',
-        cell: ({row}) => getSectionTimeLocationField(row, 'endTime')
-
-      },
-      {
-        id: 'room',
-        header: 'Room',
-        cell: ({row}) => getSectionTimeLocationField(row, 'room')
-      },
-      {
-        id: 'instructor',
-        header: 'Instructor',
-        cell: ({row}) => getSectionInstructors(row)
-      },
-      // You can add more columns for secondary sections here,
-      // and they will automatically apply to subRows as well if the accessor matches.
-    ],
-    []
-  );
+function PrimaryTable({ data, columns, testId = "PrimaryTable" }) {
 
   const [expanded, setExpanded] = useState({});
+
+  const altColor = "#e3ebfc";
 
   const table = useReactTable({
     data: data,
@@ -136,58 +24,64 @@ function PrimaryTable({testId = "PrimaryTable" }) {
     getCoreRowModel: getCoreRowModel(),
     getExpandedRowModel: getExpandedRowModel(), // Required for expansion
   });
- 
+
   return (
-    <>
-      <table data-testid={testId} className="table">
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id} colSpan={header.colSpan}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={`rowId-${row.id}`}>
+    <Table data-testid={testId} bordered hover className="table-hover">
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id} colSpan={header.colSpan}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+              </th>
+            ))}
+          </tr>
+        ))}˝
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => {
+          const rowStyle = { backgroundColor: row.index % 2 === 0 ? altColor : "white"};
+          return (
+            <tr key={`rowId-${row.id}`} style={rowStyle} >
               {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
+                <td key={cell.id}
+                  style={{
+                    backgroundColor: "inherit",
+                    fontWeight: row.depth == 0
+                      ? "bold"
+                      : "normal",
+                  }}
+                >
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
               ))}
             </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          {table.getFooterGroups().map((footerGroup) => (
-            <tr key={footerGroup.id}>
-              {footerGroup.headers.map((header) => (
-                <th key={header.id} colSpan={header.colSpan}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                      header.column.columnDef.footer,
-                      header.getContext()
-                    )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot>
-      </table>
-      <pre className="text-sm">
-        {JSON.stringify(table.getState().expanded, null, 2)}
-      </pre>
-    </>
+          )
+        }
+        )}
+      </tbody>
+      <tfoot>
+        {table.getFooterGroups().map((footerGroup) => (
+          <tr key={footerGroup.id}>
+            {footerGroup.headers.map((header) => (
+              <th key={header.id} colSpan={header.colSpan}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                    header.column.columnDef.footer,
+                    header.getContext()
+                  )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </tfoot>
+    </Table>
   );
 }
 
--- frontend/src/main/components/Courses/CoursePrimaryTable.js
+++ frontend/src/main/components/Courses/CoursePrimaryTable.js
@@ -1,106 +1,120 @@
 import React from "react";
 import PrimaryTable from "main/components/Common/PrimaryTable";
 
-// import { yyyyqToQyy } from "main/utils/quarterUtilities.js";
+import { yyyyqToQyy } from "main/utils/quarterUtilities.js";
 
-export default function CoursePrimaryTable({ primaries }) {
+import {
+  convertToFraction,
+  formatDays,
+  formatInstructors,
+  formatLocation,
+  formatTime,
+} from "main/utils/sectionUtils.js";
 
-  const columns = [
-    {
-      id: 'expander', // Unique ID for the expander column
-      header: ({ table }) => (
-        <button
-          {...{
-            onClick: () => { 
-              table.getToggleAllRowsExpandedHandler();
-            },
-          }}
-        >
-          {table.getIsAllRowsExpanded() ? '-' : '+'}
-        </button>
-      ),
-      cell: ({ row }) => {
-        console.log(`Row ID: ${row.id}, Can Expand: ${row.getCanExpand()}, Is Expanded: ${row.getIsExpanded()}`);
-        return row.getCanExpand() ? (
+export function getSection(row) {
+  return ('primary' in row.original) ? row.original.primary : row.original;
+}
+
+export function getSectionField(row, key) {
+  return getSection(row)[key];
+}
+
+export function enrollmentFraction(row) {
+  const num = getSectionField(row, 'enrolledTotal') 
+  const denom = getSectionField(row, 'maxEnroll')
+  const result = convertToFraction(num, denom);
+  return result;
+}
+
+
+    
+
+
+export default function CoursePrimaryTable({ primaries, testId="CoursePrimaryTable"}) {
+  
+  const columns = React.useMemo(
+    () => [
+      {
+        id: 'expander', // Unique ID for the expander column
+        header: ({ table }) => (
           <button
+            data-testId={`${testId}-expand-all-rows`}
             {...{
-              onClick: () => {
-                row.getToggleExpandedHandler();
-              },
-              style: { cursor: 'pointer' },
+              onClick: table.getToggleAllRowsExpandedHandler(),
             }}
           >
-            {row.getIsExpanded() ? '-' : '+'}
+            {table.getIsAllRowsExpanded() ? "➖" : "➕"} 
           </button>
-        ) : (
-          '🔵' // Or null, or an empty span for rows that can't expand
-        )},
-      // This is important for indenting sub-rows
-      // We'll apply this style in the render, but you can define it here too
-      // For sub-rows, you might want to adjust cell content for clarity
-    },
-    {
-      accessorKey: 'courseId',
-      header: 'Course ID',
-      cell: ({ row, getValue }) => (
-        <div style={{ paddingLeft: `${row.depth * 2}rem` }}>
-          {getValue()}
-        </div>
-      ),
-    },
-    {
-      accessorKey: 'title',
-      header: 'Title',
-    },
-    {
-      accessorKey: 'enrollCode',
-      accessorFn: (row) => row.primary.enrollCode,
-      header: 'Enroll Code',
-    },
-    {
-      accessorKey: 'section',
-      accessorFn: (row) => row.primary.section,
-      header: 'Section',
-    },
-    {
-      accessorKey: 'enrolledTotal',
-      accessorFn: (row) => row.primary.enrolledTotal,
-      header: 'Enrolled',
-    },
-    {
-      accessorKey: 'maxEnroll',
-      accessorFn: (row) => row.primary.maxEnroll,
-      header: 'Max Enroll',
-    },
-    {
-      accessorFn: row => row.primary.timeLocations?.[0]?.days, // Accessing nested data for primary row
-      id: 'days',
-      header: 'Days',
-    },
-    {
-      accessorFn: row => row.primary.timeLocations?.[0]?.beginTime,
-      id: 'beginTime',
-      header: 'Begin Time',
-    },
-    {
-      accessorFn: row => row.primary.timeLocations?.[0]?.endTime,
-      id: 'endTime',
-      header: 'End Time',
-    },
-    {
-      accessorFn: row => row.primary.timeLocations?.[0]?.room,
-      id: 'room',
-      header: 'Room',
-    },
-    {
-      accessorFn: row => row.primary.instructors?.[0]?.instructor,
-      id: 'instructor',
-      header: 'Instructor',
-    },
-    // You can add more columns for secondary sections here,
-    // and they will automatically apply to subRows as well if the accessor matches.
-  ];
-
+        ),
+        cell: ({ row }) =>
+          row.getCanExpand() ? (
+            <button
+              data-testId={`${testId}-row-${row.index}-expand-button`}
+              {...{
+                onClick: row.getToggleExpandedHandler(),
+                style: { cursor: 'pointer' },
+              }}
+            >
+              {row.getIsExpanded() ? "➖" : "➕"}
+            </button>
+          ) : (
+            <span data-testid={`${testId}-row-${row.index}-cannot-expand`} /> 
+          ),
+        // This is important for indenting sub-rows
+        // We'll apply this style in the render, but you can define it here too
+        // For sub-rows, you might want to adjust cell content for clarity
+      },
+      {
+        accessorKey: 'courseId',
+        header: 'Course ID',
+        cell: ({ row, getValue }) => (
+          <div style={{ paddingLeft: `${row.depth * 2}rem` }}>
+            {getValue()}
+          </div>
+        ),
+      },
+      {
+        header: "Quarter",
+        accessorKey: "quarter",
+        cell: ({ row }) => row.original.quarter ? yyyyqToQyy(row.original.quarter) : "",
+      },
+      {
+        accessorKey: 'title',
+        header: 'Title',
+      },
+      {
+        header: "Enrolled",
+        accessorKey: "enrolled",
+        cell: ({ row }) => enrollmentFraction(row),
+      },
+      {
+        id: 'location',
+        header: 'Location',
+        cell: ({ row }) => formatLocation(getSection(row).timeLocations)
+      },
+      {
+        id: 'days',
+        header: "Days",
+        cell: ({row}) => formatDays(getSection(row).timeLocations)
+      },
+      {
+        id: 'time',
+        header: "Time",
+        cell: ({row}) => formatTime(getSection(row).timeLocations)
+      },
+      {
+        id: "instructor",
+        header: "Instructor",
+        cell: ({row}) => formatInstructors(getSection(row).instructors)
+      },
+      {
+        accessorKey: 'enrollCode',
+        header: 'Enroll Code',
+        cell: ({ row }) => getSectionField(row, "enrollCode")
+      },
+    ],
+    [testId]
+  );
 
   return (
     <PrimaryTable data={primaries} columns={columns} testid={"CoursePrimaryTable"} />
