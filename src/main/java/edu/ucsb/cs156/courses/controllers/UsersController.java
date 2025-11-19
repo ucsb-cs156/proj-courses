0f143715c623d64f50a30901700a1a8e756235b2
Agam Makkar <agamakkar@gmail.com>
2025-11-18 17:23:42 -0800

    am - added new backend endpoint that is paged

--- src/main/java/edu/ucsb/cs156/courses/controllers/UsersController.java
+++ src/main/java/edu/ucsb/cs156/courses/controllers/UsersController.java
@@ -5,12 +5,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.ucsb.cs156.courses.entities.User;
 import edu.ucsb.cs156.courses.repositories.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "User information (admin only)")
@@ -28,5 +35,63 @@ public class UsersController extends ApiController {
     Iterable<User> users = userRepository.findAll();
     String body = mapper.writeValueAsString(users);
     return ResponseEntity.ok().body(body);
+  }
+
+  @Operation(summary = "Get a paginated list of users")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  @GetMapping(value = "/paginated", produces = "application/json")
+  public Page<User> getUsersPaginated(
+      @Parameter(
+              name = "page",
+              description = "what page of the data",
+              example = "0",
+              required = true)
+          @RequestParam
+          int page,
+      @Parameter(
+              name = "pageSize",
+              description = "size of each page",
+              example = "10",
+              required = true)
+          @RequestParam
+          int pageSize,
+      @Parameter(
+              name = "sortField",
+              description = "sort field",
+              example = "email",
+              required = false)
+          @RequestParam(defaultValue = "id")
+          String sortField,
+      @Parameter(
+              name = "sortDirection",
+              description = "sort direction",
+              example = "ASC",
+              required = false)
+          @RequestParam(defaultValue = "ASC")
+          String sortDirection) {
+
+    List<String> allowedSortFields = Arrays.asList("id", "email", "givenName", "familyName");
+
+    if (!allowedSortFields.contains(sortField)) {
+      throw new IllegalArgumentException(
+          String.format(
+              "%s is not a valid sort field. Valid values are %s", sortField, allowedSortFields));
+    }
+
+    List<String> allowedSortDirections = Arrays.asList("ASC", "DESC");
+    if (!allowedSortDirections.contains(sortDirection)) {
+      throw new IllegalArgumentException(
+          String.format(
+              "%s is not a valid sort direction. Valid values are %s",
+              sortDirection, allowedSortDirections));
+    }
+
+    Direction sortDirectionObject = Direction.ASC;
+    if (sortDirection.equals("DESC")) {
+      sortDirectionObject = Direction.DESC;
+    }
+
+    PageRequest pageRequest = PageRequest.of(page, pageSize, sortDirectionObject, sortField);
+    return userRepository.findAll(pageRequest);
   }
 }
--- src/test/java/edu/ucsb/cs156/courses/controllers/UsersControllerTests.java
+++ src/test/java/edu/ucsb/cs156/courses/controllers/UsersControllerTests.java
@@ -13,11 +13,16 @@ import edu.ucsb.cs156.courses.repositories.UserRepository;
 import edu.ucsb.cs156.courses.testconfig.TestConfig;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -27,6 +32,15 @@ import org.springframework.test.web.servlet.MvcResult;
 public class UsersControllerTests extends ControllerTestCase {
 
   @MockBean UserRepository userRepository;
+
+  ArrayList<User> emptyArray = new ArrayList<User>();
+  PageRequest pageRequest_0_10_ASC_id = PageRequest.of(0, 10, Direction.ASC, "id");
+  PageRequest pageRequest_0_10_DESC_email = PageRequest.of(0, 10, Direction.DESC, "email");
+
+  private final Page<User> emptyPage_0_10_ASC_id =
+      new PageImpl<User>(emptyArray, pageRequest_0_10_ASC_id, 0);
+  private final Page<User> emptyPage_0_10_DESC_email =
+      new PageImpl<User>(emptyArray, pageRequest_0_10_DESC_email, 0);
 
   @Test
   public void users__logged_out() throws Exception {
@@ -65,5 +79,109 @@ public class UsersControllerTests extends ControllerTestCase {
     verify(userRepository, times(1)).findAll();
     String responseString = response.getResponse().getContentAsString();
     assertEquals(expectedJson, responseString);
+  }
+
+  @Test
+  public void getUsersPaginated__logged_out() throws Exception {
+    mockMvc
+        .perform(get("/api/admin/users/paginated?page=0&pageSize=10"))
+        .andExpect(status().is(403));
+  }
+
+  @WithMockUser(roles = {"USER"})
+  @Test
+  public void getUsersPaginated__user_logged_in() throws Exception {
+    mockMvc
+        .perform(get("/api/admin/users/paginated?page=0&pageSize=10"))
+        .andExpect(status().is(403));
+  }
+
+  @WithMockUser(roles = {"ADMIN", "USER"})
+  @Test
+  public void getUsersPaginated__admin_logged_in__empty_page_default_sort() throws Exception {
+    // arrange
+    when(userRepository.findAll(pageRequest_0_10_ASC_id)).thenReturn(emptyPage_0_10_ASC_id);
+
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(get("/api/admin/users/paginated?page=0&pageSize=10"))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    // assert
+    String expectedResponseAsJson = mapper.writeValueAsString(emptyPage_0_10_ASC_id);
+    String actualResponse = response.getResponse().getContentAsString();
+    assertEquals(expectedResponseAsJson, actualResponse);
+  }
+
+  @WithMockUser(roles = {"ADMIN", "USER"})
+  @Test
+  public void getUsersPaginated__admin_logged_in__empty_page_DESC_email_sort() throws Exception {
+    // arrange
+    when(userRepository.findAll(pageRequest_0_10_DESC_email)).thenReturn(emptyPage_0_10_DESC_email);
+
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(
+                get("/api/admin/users/paginated?page=0&pageSize=10&sortField=email&sortDirection=DESC"))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    // assert
+    String expectedResponseAsJson = mapper.writeValueAsString(emptyPage_0_10_DESC_email);
+    String actualResponse = response.getResponse().getContentAsString();
+    assertEquals(expectedResponseAsJson, actualResponse);
+  }
+
+  @WithMockUser(roles = {"ADMIN"})
+  @Test
+  public void getUsersPaginated__invalid_sort_field() throws Exception {
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(
+                get(
+                    "/api/admin/users/paginated?page=0&pageSize=10&sortField=invalid&sortDirection=ASC"))
+            .andExpect(status().isBadRequest())
+            .andReturn();
+
+    // assert
+    Map<String, String> expectedResponse =
+        Map.of(
+            "message",
+            "invalid is not a valid sort field. Valid values are [id, email, givenName, familyName]",
+            "type",
+            "IllegalArgumentException");
+
+    String expectedResponseAsJson = mapper.writeValueAsString(expectedResponse);
+    String actualResponse = response.getResponse().getContentAsString();
+    assertEquals(expectedResponseAsJson, actualResponse);
+  }
+
+  @WithMockUser(roles = {"ADMIN"})
+  @Test
+  public void getUsersPaginated__invalid_sort_direction() throws Exception {
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(
+                get(
+                    "/api/admin/users/paginated?page=0&pageSize=10&sortField=id&sortDirection=INVALID"))
+            .andExpect(status().isBadRequest())
+            .andReturn();
+
+    // assert
+    Map<String, String> expectedResponse =
+        Map.of(
+            "message",
+            "INVALID is not a valid sort direction. Valid values are [ASC, DESC]",
+            "type",
+            "IllegalArgumentException");
+
+    String expectedResponseAsJson = mapper.writeValueAsString(expectedResponse);
+    String actualResponse = response.getResponse().getContentAsString();
+    assertEquals(expectedResponseAsJson, actualResponse);
   }
 }
