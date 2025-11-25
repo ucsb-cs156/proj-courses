8e18fb051d340b1dc2f27cd985469a6ae9e7206c
Nathan Ngo <nathan_ngo@ucsb.edu>
2025-11-24 23:46:01 -0800

    nn - fix test coverage coursescsvcontroller

--- src/test/java/edu/ucsb/cs156/courses/controllers/CoursesCSVControllerTests.java
+++ src/test/java/edu/ucsb/cs156/courses/controllers/CoursesCSVControllerTests.java
@@ -22,16 +22,18 @@ import edu.ucsb.cs156.courses.testconfig.TestConfig;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.mockito.Answers;
 import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MvcResult;
 
-@WebMvcTest(controllers = {CoursesCSVController.class})
+@SpringBootTest(classes = edu.ucsb.cs156.courses.CoursesApplication.class)
+@AutoConfigureMockMvc
 @Import(TestConfig.class)
 @AutoConfigureDataJpa
 public class CoursesCSVControllerTests extends ControllerTestCase {
