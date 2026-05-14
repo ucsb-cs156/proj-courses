b24da99e15406bb813ab27b9e862fbd908aec8c2
David Chen <dchen451@ucsb.edu>
2026-05-14 01:39:27 -0700

    dc - formatted JobsControllerTests

--- src/test/java/edu/ucsb/cs156/courses/controllers/JobsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/courses/controllers/JobsControllerTests.java
@@ -32,13 +32,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MvcResult;
 
 @Slf4j
