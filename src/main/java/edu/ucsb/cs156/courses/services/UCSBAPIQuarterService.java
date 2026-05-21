842939b49d85378b6aa3437b7d5c2a69d826df3d
Andrew Bryan <andrewbryan@ucsb.edu>
2026-05-20 17:52:39 -0700

    Added caching to getCurrentQuarter

--- src/main/java/edu/ucsb/cs156/courses/services/UCSBAPIQuarterService.java
+++ src/main/java/edu/ucsb/cs156/courses/services/UCSBAPIQuarterService.java
@@ -13,12 +13,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -91,7 +93,8 @@ public class UCSBAPIQuarterService {
     return result;
   }
 
-  // AB - Maybe should cache this?
+  @Cacheable
+  @Scheduled(cron = "0 0 0 * * *")
   public UCSBAPIQuarter getCurrentQuarter() throws Exception {
     HttpHeaders headers = new HttpHeaders();
     headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
@@ -164,7 +167,7 @@ public class UCSBAPIQuarterService {
 
   public boolean quarterYYYYQInRange(String quarterYYYYQ) {
     boolean dateGEStart = quarterYYYYQ.compareTo(startQtrYYYYQ) >= 0;
-    boolean dateLEEnd = quarterYYYYQ.compareTo(endQtrYYYYQ) <= 0;
+    boolean dateLEEnd = quarterYYYYQ.compareTo(getEndQtrYYYYQ()) <= 0;
     return (dateGEStart && dateLEEnd);
   }
 
